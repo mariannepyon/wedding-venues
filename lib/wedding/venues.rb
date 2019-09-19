d79a80bf51a2e853c59d0b881cb0dde32e4ec494
Marianne Pyon <mariannepyon@gmail.com>
2019-09-19 20:25:41 +0000

    first commit

---
+++ lib/wedding/venues.rb
@@ -0,0 +1,8 @@
+require "wedding/venues/version"
+
+module Wedding
+  module Venues
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/wedding/venues/version.rb
@@ -0,0 +1,5 @@
+module Wedding
+  module Venues
+    VERSION = "0.1.0"
+  end
+end
