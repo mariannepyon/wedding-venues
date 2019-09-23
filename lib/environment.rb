e9f022e2edf05a34884bc33445a9b40a148cec19
Marianne Pyon <mariannepyon@gmail.com>
2019-09-23 20:48:00 +0000

    include venue in environment

--- lib/environment.rb
+++ lib/environment.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 
 require_relative "./wedding-venues/version"
 require_relative "./wedding-venues/CLI"
+require_relative "./wedding-venues/venue"
 
 module WeddingVenues
     class Error < StandardError; end
