7c6a635f3f0b9f012c55a98dd4eb397370a1147d
Marianne Pyon <mariannepyon@gmail.com>
2019-09-23 14:22:03 +0000

    edit gemspec for pry

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "./wedding-venues/version"
 require_relative "./wedding-venues/CLI"
 
+require 'pry'
+
 module WeddingVenues
     class Error < StandardError; end
     # Your code goes here...
