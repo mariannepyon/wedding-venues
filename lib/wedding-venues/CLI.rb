d2f705a19fbf7b983eb316e4526dd449245767bb
Marianne Pyon <mariannepyon@gmail.com>
2019-09-24 13:34:51 +0000

    changed self.eastbay

--- lib/wedding-venues/CLI.rb
+++ lib/wedding-venues/CLI.rb
@@ -11,14 +11,9 @@ class WeddingVenues::CLI
   def list_venues
     puts "Wedding venues in East Bay, CA:"
     
-    # puts <<-DOC.gsub /^\s*/, ''
-    #   1. Fairview Metropolitan - Oakland, CA - $5,837 - https://www.wedding-spot.com/venue/1070/Fairview-Metropolitan/
-    #   2. Canyon View - San Ramon, CA - $7,324 - https://www.wedding-spot.com/venue/82/Canyon-View/
-    #   3. Rios Lovell Estate Winery - Livermore, CA - $15,980 - https://www.wedding-spot.com/venue/57/Rios-Lovell-Estate-Winery-Rios-Reserve-Estate/
-    # DOC
     @venue = WeddingVenues::Venue.eastbay
     @venue.each.with_index(1) do |deal, i|
-      puts "#{i}, #{venue.name} - #{venue.location} - #{venue.price} - #{venue.url}"
+      puts "#{i}. #{venue.name} - #{venue.location} - #{venue.price} - #{venue.url}"
     end
   end
   
