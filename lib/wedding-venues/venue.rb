8d612e9e969e79d1695a9534b2c82865fbaba013
Marianne Pyon <mariannepyon@gmail.com>
2019-09-25 18:10:51 +0000

    change scrape

--- lib/wedding-venues/venue.rb
+++ lib/wedding-venues/venue.rb
@@ -14,11 +14,12 @@ class WeddingVenues::Venue
   def self.scrape_weddingspot
     doc = Nokogiri::HTML(open("https://www.wedding-spot.com/wedding-venues/"))
     
-    # doc.css(".venue-box-content-with-budget").each do |venue|
-    #   name = venue.css(".venue-name").text
-    #   location = venue.css(".venue-region").text.strip
-    #   description = venue.css(".venue-description").text.strip
-    #   url = venue.css("a.venu-link").first.attr("href")
+    doc.css(".venue-box-content-with-budget").each do |venue|
+      name = venue.css(".venue-name").text
+      location = venue.css(".venue-region").text.strip
+      description = venue.css(".venue-description").text.strip
+      url = venue.css("a.venue-link").first.attr("href")
+      Venue.new(name, location, description, url)
       binding.pry
     
  # end
