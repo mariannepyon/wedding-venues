ed0b3710aaa96ccf1d97ae6c2f663c676e8d32aa
Marianne Pyon <mariannepyon@gmail.com>
2019-09-25 20:43:54 +0000

    edit CLI

--- lib/wedding-venues/CLI.rb
+++ lib/wedding-venues/CLI.rb
@@ -3,37 +3,48 @@
 class WeddingVenues::CLI
   
   def call
+    WeddingVenues::Scraper.new.make_venues
     puts "Wedding venues in East Bay, CA:"
-    list_venues
-    menu
-    goodbye
+    start
+    # list_venues
+    # menu
+    # goodbye
   end
   
-  def list_venues
-    @venue = WeddingVenues::Venue.eastbay
-    @venue.each.with_index(1) do |venue, i|
-      puts "#{i}. #{venue.name} - #{venue.location} - #{venue.url}"
-    end
-  end
+  def start
+    puts ""
+    puts "What number venue would you like to see?"
+    input = gets.strip.to_i
+
+    print_venues(input)
   
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Enter the number of the venue you'd like more info on or type list to see the venues again or exit:"
-      input = gets.strip.downcase
+  
+  
+#   def list_venues
+#     @venue = WeddingVenues::Venue.eastbay
+#     @venue.each.with_index(1) do |venue, i|
+#       puts "#{i}. #{venue.name} - #{venue.location} - #{venue.url}"
+#     end
+#   end
+  
+#   def menu
+#     input = nil
+#     while input != "exit"
+#       puts "Enter the number of the venue you'd like more info on or type list to see the venues again or exit:"
+#       input = gets.strip.downcase
       
-      if input.to_i > 0
-        the_venue = @venue[input.to_i-1]
-        puts "#{the_venue.name} - #{the_venue.location} - #{the_venue.url}"
-      elsif input == "list"
-        list_venues
-      else
-        puts "Option not found, type list or exit."
-      end
-    end
-  end
+#       if input.to_i > 0
+#         the_venue = @venue[input.to_i-1]
+#         puts "#{the_venue.name} - #{the_venue.location} - #{the_venue.url}"
+#       elsif input == "list"
+#         list_venues
+#       else
+#         puts "Option not found, type list or exit."
+#       end
+#     end
+#   end
   
-  def goodbye
-    puts "Thank you for visiting!"
-  end
-end
+#   def goodbye
+#     puts "Thank you for visiting!"
+#   end
+# end
