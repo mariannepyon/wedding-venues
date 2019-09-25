64228310499ae979073e8d117db298d674b10909
Marianne Pyon <mariannepyon@gmail.com>
2019-09-25 20:15:20 +0000

    venue items

--- lib/wedding-venues/venue.rb
+++ lib/wedding-venues/venue.rb
@@ -15,12 +15,14 @@ class WeddingVenues::Venue
   def self.scrape_weddingspot
     doc = Nokogiri::HTML(open("https://www.wedding-spot.com/wedding-venues/"))
     
-      doc.css(".venue-box-content-with-budget").each do |venue|
-        name = venue.css(".venue-name").text
-        location = venue.css(".venue-region").text.strip
-        url = venue.css("a.venue-link").first.attr("href")
+    venue = self.new
+      #doc.css(".venue-box-content-with-budget").each do |venue|
+        venue.name = doc.search("div.venue-box-content-with-budget .venue-name").text
+        venue.location = doc.search("div.venue-box-content-with-budget .venue-region").text.strip
+        venue.url = doc.search("a.venue-link").first.attr("href").strip
         
-      Venue.new(name, location, description, url)
+      #Venue.new(name, location, url)
+      venue
       
   end
 end
