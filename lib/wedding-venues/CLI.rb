8dcce829db597e7991e5fcd72c4d9fc6b18a9330
Marianne Pyon <mariannepyon@gmail.com>
2019-09-25 20:48:12 +0000

    edit CLI2

--- lib/wedding-venues/CLI.rb
+++ lib/wedding-venues/CLI.rb
@@ -12,12 +12,59 @@ class WeddingVenues::CLI
   end
   
   def start
+    
     puts ""
-    puts "What number venue would you like to see?"
+    puts "What venue would you like more information on?"
     input = gets.strip.to_i
-
-    print_venues(input)
+    
+    venue = WeddingVenues::Venue.find(input.to_i)
   
+    print_venue(venue)
+
+    puts ""
+    puts "Would you like to see another venue? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts ""
+      puts "Thank you! Have a great day!"
+      exit
+    else
+      puts ""
+      puts "I don't understand that answer."
+      start
+    end
+  end
+
+  def print_venue(venue)
+    puts ""
+    puts "----------- #{venue.name}-----------"
+    puts ""
+    puts "Location:           #{venue.location}"
+    puts "Head Chef:          #{restaurant.head_chef}"
+    puts "Style of Food:      #{restaurant.food_style}"
+    puts "Standout Dish:      #{restaurant.best_dish}"
+    puts "Contact:            #{restaurant.contact}"
+    puts "Website:            #{restaurant.website_url}"
+    puts ""
+    puts "---------------Description--------------"
+    puts ""
+    puts "#{restaurant.description}"
+    puts ""
+  end
+
+  def print_restaurants(from_number)
+    puts ""
+    puts "---------- Restaurants #{from_number} - #{from_number+9} ----------"
+    puts ""
+    WorldsBestRestaurants::Restaurant.all[from_number-1, 10].each.with_index(from_number) do |restaurant, index|
+      puts "#{index}. #{restaurant.name} - #{restaurant.location}"
+    end
+  end
+
+end
   
   
 #   def list_venues
