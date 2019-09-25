30b4717461c8c215b859e389ee744ba9b5b1f1c5
Marianne Pyon <mariannepyon@gmail.com>
2019-09-25 20:26:00 +0000

    created a scraper page2

--- lib/wedding-venues/scraper.rb
+++ lib/wedding-venues/scraper.rb
@@ -4,13 +4,13 @@ class WeddingVenues::Scraper
     Nokogiri::HTML(open("https://www.wedding-spot.com/wedding-venues/"))
   end
 
-  def scrape_restaurants_index
-     self.get_page.css("div#t1-50 li")
+  def scrape_venues_index
+     self.get_page.css("div#list-view")
   end
 
   def make_venues
-    scrape_venues_index.each do |r|
-      WeddingVenues::Venue.new_from_index_page(r)
+    scrape_venues_index.each do |v|
+      WeddingVenues::Venue.new_from_index_page(v)
     end
   end
 end
