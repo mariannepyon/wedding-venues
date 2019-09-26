f9a1c425a6160b5d4bbfb16c58d19f2474da6310
Marianne Pyon <mariannepyon@gmail.com>
2019-09-26 17:45:37 +0000

    working

--- lib/wedding-venues/CLI.rb
+++ lib/wedding-venues/CLI.rb
@@ -4,10 +4,10 @@ class WeddingVenues::CLI
   
   def call
     WeddingVenues::Scraper.new.make_venues
-    puts "Congratulations on your engagement!Welcome to wedding venues!"
+    puts "Congratulations on your engagement! Welcome to Northern California wedding venues!"
     list_venues
     venue_information
-    # goodbye
+    goodbye
   end
   
   def list_venues
@@ -19,34 +19,53 @@ class WeddingVenues::CLI
   
   
   def venue_information
-    puts ""
-    puts "Which venue would you like more information on? Please write the number:"
+    
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the venue you would like more information on or type 'list' to see the venues again or 'exit' to leave:"
+      input = gets.strip.downcase
 
-    input = gets.strip.to_i
-    
-    venue = WeddingVenues::Venue.find(input.to_i)
-    
-    print_venue(venue)
-    
-    puts ""
-    puts "Would you like to see another venue? Enter Y or N"
-
-    input = gets.strip.downcase
-    if input == "y"
-      list_venues
-      venue_information
-    elsif input == "n"
-      puts ""
-      puts "Thank you! Have a great day!"
-      exit
-    else
-      puts ""
-      puts "I don't understand that answer."
-      venue_information
+      if input.to_i > 0 
+        venue = WeddingVenues::Venue.find(input.to_i)
+        print_venue(venue)
+      elsif input == "list"
+        list_venues
+      else
+        puts ""
+        puts "Not sure what you want, type 'list' or 'exit'."
+        list_venues
+      end
     end
+    
+    
+  #   puts ""
+  #   puts "Which venue would you like more information on? Please write the number:"
+
+  #   input = gets.strip.to_i
+    
+  #   venue = WeddingVenues::Venue.find(input.to_i)
+    
+  #   print_venue(venue)
+    
+  #   puts ""
+  #   puts "Would you like to see another venue? Enter Y or N"
+
+  #   input = gets.strip.downcase
+  #   if input == "y"
+  #     list_venues
+  #     venue_information
+  #   elsif input == "n"
+  #     puts ""
+  #     puts "Thank you! Have a great day!"
+  #     exit
+  #   else
+  #     puts ""
+  #     puts "I don't understand that answer."
+  #     venue_information
+  #   end
   end
 
-   def print_venue(venue)
+  def print_venue(venue)
       puts ""
       puts "----------- #{venue.name} ------------------"
       puts ""
@@ -82,7 +101,6 @@ class WeddingVenues::CLI
 #     end
 #   end
   
-#   def goodbye
-#     puts "Thank you for visiting!"
-#   end
-# end
+  def goodbye
+    puts "Thank you! Have a great day!"
+  end
