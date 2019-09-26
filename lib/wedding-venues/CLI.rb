53604ceddab4203bcc3ae499c0a5f223b1c199ef
Marianne Pyon <mariannepyon@gmail.com>
2019-09-26 18:04:26 +0000

    donish

--- lib/wedding-venues/CLI.rb
+++ lib/wedding-venues/CLI.rb
@@ -7,7 +7,9 @@ class WeddingVenues::CLI
     puts "Congratulations on your engagement! Welcome to Northern California wedding venues!"
     list_venues
     venue_information
-    goodbye
+    # puts ""
+    # puts "Thank you! Have a great day!"
+    # exit
   end
   
   def list_venues
@@ -19,50 +21,31 @@ class WeddingVenues::CLI
   
   
   def venue_information
-    
-    input = nil
-    while input != "exit"
-      puts "Enter the number of the venue you would like more information on or type 'list' to see the venues again or 'exit' to leave:"
-      input = gets.strip.downcase
+    puts ""
+    puts "Which venue would you like more information on? Please write the number:"
 
-      if input.to_i > 0 
-        venue = WeddingVenues::Venue.find(input.to_i)
-        print_venue(venue)
-      elsif input == "list"
-        list_venues
-      else
-        puts ""
-        puts "Not sure what you want, type 'list' or 'exit'."
-        list_venues
-      end
+    input = gets.strip.to_i
+    
+    venue = WeddingVenues::Venue.find(input.to_i)
+    
+    print_venue(venue)
+    
+    puts ""
+    puts "Would you like to see another venue? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      list_venues
+      venue_information
+    elsif input == "n"
+      puts ""
+      puts "Thank you! Have a magical day!"
+      exit
+    else
+      puts ""
+      puts "I don't understand that answer."
+      venue_information
     end
-    
-    
-  #   puts ""
-  #   puts "Which venue would you like more information on? Please write the number:"
-
-  #   input = gets.strip.to_i
-    
-  #   venue = WeddingVenues::Venue.find(input.to_i)
-    
-  #   print_venue(venue)
-    
-  #   puts ""
-  #   puts "Would you like to see another venue? Enter Y or N"
-
-  #   input = gets.strip.downcase
-  #   if input == "y"
-  #     list_venues
-  #     venue_information
-  #   elsif input == "n"
-  #     puts ""
-  #     puts "Thank you! Have a great day!"
-  #     exit
-  #   else
-  #     puts ""
-  #     puts "I don't understand that answer."
-  #     venue_information
-  #   end
   end
 
   def print_venue(venue)
@@ -73,34 +56,4 @@ class WeddingVenues::CLI
       puts "Website:            #{venue.url}"
       puts ""
     end
-  end
-
-
-    
-#   def list_venues
-#     @venue = WeddingVenues::Venue.new.make_venues
-#     @venue.each.with_index(1) do |venue, i|
-#       puts "#{i}. #{venue.name} - #{venue.location} - #{venue.url}"
-#     end
-#   end
-  
-#   def menu
-#     input = nil
-#     while input != "exit"
-#       puts "Enter the number of the venue you'd like more info on or type list to see the venues again or exit:"
-#       input = gets.strip.downcase
-      
-#       if input.to_i > 0
-#         the_venue = @venue[input.to_i-1]
-#         puts "#{the_venue.name} - #{the_venue.location} - #{the_venue.url}"
-#       elsif input == "list"
-#         list_venues
-#       else
-#         puts "Option not found, type list or exit."
-#       end
-#     end
-#   end
-  
-  def goodbye
-    puts "Thank you! Have a great day!"
   end
